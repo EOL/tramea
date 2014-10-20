34b25eed0f45d04738b80c9794474f4fba741f30
JRice <jrice@eol.org>
2014-10-20 14:58:00 -0400

    Added attribution to translations

--- db/migrate/20141013173816_create_names.rb
+++ db/migrate/20141013173816_create_names.rb
@@ -2,6 +2,7 @@ class CreateNames < ActiveRecord::Migration
   def change
     create_table :names do |t|
       t.string :string
+      t.string :location
       t.integer :type
       t.integer :locale
       t.boolean :preview
