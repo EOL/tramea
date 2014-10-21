f9ce28da8d4415f8ba8f33788b6e5721991a1072
JRice <jrice@eol.org>
2014-10-21 14:53:27 -0400

    Fixed reserved enum names, rebuilt svg

--- app/models/node.rb
+++ app/models/node.rb
@@ -13,17 +13,20 @@ class Node < ActiveRecord::Base
   #   sub("sub_", "sub").sub("super_", "super") }.sort.uniq
   # NOTE: I also removed things that I thought weren't ranks and things that
   # were abbreviations or duplicates; this list WILL change once we hear from SPG.
-  enum rank: %q[
-    class cohort convar cultivar division family form forma genus hybrid
-    hybrid_formula informal infraclass infradivision infrakingdom infraorder
-    infraspecies infraspecificname infrasubspecificname kingdom lusus microgene
-    monster mutant nothoform nothogenus nothospecies nothosubspecies nothotaxon
-    nothovariety order parvorder phylum prole pseudovariety race section series
-    species species_group species_subgroup status stirps subclass subdivision
-    subfamily subform subgenus subkingdom suborder subphylum subsection
-    subseries subspecies subsubf subtaxon subtribe subvariety sugenus
-    supercohort superclass superfamily superkingdom superorder superphylum
-    supertribe tax_vag taxon tribe unranked variety
-  ].map(&:to_sym)
+  # NOTE: added _rank to those which use reserved names. :|
+  enum rank: [
+    :class_rank, :cohort, :convar, :cultivar, :division, :family, :form, :forma,
+    :genus, :hybrid, :hybrid_formula, :informal, :infraclass, :infradivision,
+    :infrakingdom, :infraorder, :infraspecies, :infraspecificname,
+    :infrasubspecificname, :kingdom, :lusus, :microgene, :monster, :mutant,
+    :nothoform, :nothogenus, :nothospecies, :nothosubspecies, :nothotaxon,
+    :nothovariety, :order_rank, :parvorder, :phylum, :prole, :pseudovariety, :race,
+    :section, :series, :species, :species_group, :species_subgroup, :status,
+    :stirps, :subclass, :subdivision, :subfamily, :subform, :subgenus,
+    :subkingdom, :suborder, :subphylum, :subsection, :subseries, :subspecies,
+    :subsubf, :subtaxon, :subtribe, :subvariety, :sugenus, :supercohort,
+    :superclass_rank, :superfamily, :superkingdom, :superorder, :superphylum,
+    :supertribe, :tax_vag, :taxon, :tribe, :unranked, :variety
+  ]
 
 end
