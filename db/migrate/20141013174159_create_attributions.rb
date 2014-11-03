69a3f8d0af8c7bb14339d92330eb0bceae0d095f
JRice <jrice@eol.org>
2014-11-03 07:39:05 -0500

    Mistake: medium_type should be a string.

--- db/migrate/20141013174159_create_attributions.rb
+++ db/migrate/20141013174159_create_attributions.rb
@@ -1,7 +1,7 @@
 class CreateAttributions < ActiveRecord::Migration
   def change
     create_table :attributions do |t|
-      t.integer :medium_type
+      t.string :medium_type
       t.integer :medium_id
       t.string :who
       t.string :url
