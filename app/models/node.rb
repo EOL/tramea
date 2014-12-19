4abdc548f0ea1a5a210048f62b5cc1e740738719
JRice <jrice@eol.org>
2014-12-19 09:55:51 -0500

    name for nodes is id

--- app/models/node.rb
+++ app/models/node.rb
@@ -4,7 +4,8 @@ class Node < ActiveRecord::Base
 
   has_many :associations, as: :parent
 
-  acts_as_tree dependent: :destroy
+  # NOTE: Ideally, we would use the preferred scientific name as the name_column
+  acts_as_tree dependent: :destroy, name_column: :id
 
   # NOTE: Yes, this is HUGE and obnoxious. But I do believe this is is the best
   # way to deal with ranks. :\
